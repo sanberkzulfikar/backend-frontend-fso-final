5737fa21b96b46aab90f0227e912a5dea9e89335
sanberk <tosanberk@icloud.com>
2024-04-02 03:20:21 +0300

    fix

--- index.js
+++ index.js
@@ -1,113 +1,95 @@
 const express = require("express");
 const app = express();
-var morgan = require("morgan");
 const cors = require("cors");
 
 app.use(cors());
 
-app.use(express.static("dist"));
-
-app.use(express.json());
-morgan.token("body", function (req, res) {
-  return JSON.stringify(req.body);
-});
-app.use(
-  morgan(function (tokens, req, res) {
-    return [
-      tokens.method(req, res),
-      tokens.url(req, res),
-      tokens.status(req, res),
-      tokens.res(req, res, "content-length"),
-      "-",
-      tokens["response-time"](req, res),
-      "ms",
-      tokens.body(req, res),
-    ].join(" ");
-  })
-);
-
-let persons = [
+let notes = [
   {
     id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
+    content: "HTML is easy",
+    important: true,
   },
   {
     id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
+    content: "Browser can execute only JavaScript",
+    important: false,
   },
   {
     id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: 4,
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
+    content: "GET and POST are the most important methods of HTTP protocol",
+    important: true,
   },
 ];
 
-app.get("/api/persons", (request, response) => {
-  response.json(persons);
+const requestLogger = (request, response, next) => {
+  console.log("Method:", request.method);
+  console.log("Path:  ", request.path);
+  console.log("Body:  ", request.body);
+  console.log("---");
+  next();
+};
+
+app.use(express.json());
+app.use(requestLogger);
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
+
+app.get("/", (request, response) => {
+  response.send("<h1>Hello World!</h1>");
 });
 
-app.get("/info", (request, response) => {
-  const date = new Date();
-  response.send(
-    `<p>Phonebook has info for ${persons.length} people</p><p>${date}</p>`
-  );
+app.get("/api/notes", (request, response) => {
+  response.json(notes);
 });
 
-app.get("/api/persons/:id", (request, response) => {
+app.get("/api/notes/:id", (request, response) => {
   const id = Number(request.params.id);
-  const person = persons.find((person) => person.id === id);
-  if (person) {
-    response.json(person);
+  const note = notes.find((note) => note.id === id);
+  if (note) {
+    response.json(note);
   } else {
     response.status(404).end();
   }
 });
 
 const generateId = () => {
-  const rand = Math.floor(Math.random() * 1000);
-  return rand;
+  const maxId =
+    notes.length > 0 ? Math.max(...notes.map((note) => note.id)) : 0;
+  return maxId + 1;
 };
 
-app.post("/api/persons", (request, response) => {
-  const person = request.body;
-  if (!person.name) {
+app.post("/api/notes", (request, response) => {
+  const body = request.body;
+
+  if (!body.content) {
     return response.status(400).json({
-      error: "name is missing",
-    });
-  }
-  if (!person.number) {
-    return response.status(400).json({
-      error: "number is missing",
+      error: "content missing",
     });
   }
 
-  let existing = persons.find((p) => p.name === person.name);
-  if (existing) {
-    return response.status(404).json({
-      error: "name already exists",
-    });
-  }
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    id: generateId(),
+  };
 
-  person.id = generateId();
-  persons = persons.concat(person);
-  response.json(person);
+  notes = notes.concat(note);
+  response.json(note);
 });
 
-app.delete("/api/persons/:id", (request, response) => {
+app.delete("/api/notes/:id", (request, response) => {
   const id = Number(request.params.id);
-  persons = persons.filter((person) => person.id !== id);
+  notes = notes.filter((note) => note.id !== id);
 
   response.status(204).end();
 });
 
+app.use(unknownEndpoint);
+
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
-  console.log(`Server is running on PORT ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
