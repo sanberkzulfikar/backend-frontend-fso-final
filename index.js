c655dc942bacd0f1f40c73f2aab814dfa171bb30
sanberk <tosanberk@icloud.com>
2024-04-02 03:07:11 +0300

    fix

---
+++ index.js
@@ -0,0 +1,111 @@
+const express = require("express");
+const app = express();
+var morgan = require("morgan");
+const cors = require("cors");
+
+app.use(cors());
+
+app.use(express.json());
+morgan.token("body", function (req, res) {
+  return JSON.stringify(req.body);
+});
+app.use(
+  morgan(function (tokens, req, res) {
+    return [
+      tokens.method(req, res),
+      tokens.url(req, res),
+      tokens.status(req, res),
+      tokens.res(req, res, "content-length"),
+      "-",
+      tokens["response-time"](req, res),
+      "ms",
+      tokens.body(req, res),
+    ].join(" ");
+  })
+);
+
+let persons = [
+  {
+    id: 1,
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: 2,
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: 3,
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: 4,
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
+
+app.get("/api/persons", (request, response) => {
+  response.json(persons);
+});
+
+app.get("/info", (request, response) => {
+  const date = new Date();
+  response.send(
+    `<p>Phonebook has info for ${persons.length} people</p><p>${date}</p>`
+  );
+});
+
+app.get("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  const person = persons.find((person) => person.id === id);
+  if (person) {
+    response.json(person);
+  } else {
+    response.status(404).end();
+  }
+});
+
+const generateId = () => {
+  const rand = Math.floor(Math.random() * 1000);
+  return rand;
+};
+
+app.post("/api/persons", (request, response) => {
+  const person = request.body;
+  if (!person.name) {
+    return response.status(400).json({
+      error: "name is missing",
+    });
+  }
+  if (!person.number) {
+    return response.status(400).json({
+      error: "number is missing",
+    });
+  }
+
+  let existing = persons.find((p) => p.name === person.name);
+  if (existing) {
+    return response.status(404).json({
+      error: "name already exists",
+    });
+  }
+
+  person.id = generateId();
+  persons = persons.concat(person);
+  response.json(person);
+});
+
+app.delete("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  persons = persons.filter((person) => person.id !== id);
+
+  response.status(204).end();
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Server is running on PORT ${PORT}`);
+});
