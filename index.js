213e4253bcb991a44a946edab66cd98252fa489c
sanberk <tosanberk@icloud.com>
2024-04-02 03:12:10 +0300

    fix

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const cors = require("cors");
 
 app.use(cors());
 
+app.use(express.static("dist"));
+
 app.use(express.json());
 morgan.token("body", function (req, res) {
   return JSON.stringify(req.body);
@@ -105,7 +107,7 @@ app.delete("/api/persons/:id", (request, response) => {
   response.status(204).end();
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server is running on PORT ${PORT}`);
 });
